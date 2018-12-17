338e699a313ec81965a5fec6a9d5a0c1899b5f55
Ира Ира <irakante@gmail.com>
2018-12-17 11:51:03 +0200

    ObservableCollection<Room> instead<RoomsViewModel>

--- mvvm_part2/mvvm_part2/Models/Room.cs
+++ mvvm_part2/mvvm_part2/Models/Room.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace mvvm_part2.Models
+﻿namespace mvvm_part2.Models
 {
     public class Room
     {
--- mvvm_part2/mvvm_part2/ViewModels/RoomsViewModel.cs
+++ mvvm_part2/mvvm_part2/ViewModels/RoomsViewModel.cs
@@ -1,9 +1,6 @@
 ﻿using mvvm_part2.Models;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Text;
 using System.Windows.Input;
 using Xamarin.Forms;
 
@@ -11,59 +8,66 @@ namespace mvvm_part2.ViewModels
 {
     public class RoomsViewModel : INotifyPropertyChanged
     {
-        public ICommand CreateRoomCommand { protected set; get; }       
-        public ObservableCollection<RoomsViewModel> RoomsCollection { get; set; }
-        public Room CurrentRoom { get; private set; }      
+        public ICommand CreateRoomCommand { set; get; }
+
+        private int _number;
+        public int Number
+        {
+            get => _number;
+            set
+            {
+                _number = value;
+                OnPropertyChanged(nameof(Number));
+            }
+        }
+
+        private int _freePlace;
+        public int FreePlace
+        {
+            get => _freePlace;
+            set
+            {
+                _freePlace = value;
+                OnPropertyChanged(nameof(FreePlace));
+            }
+        }
+
+        private ObservableCollection<Room> _rooms;
+        public ObservableCollection<Room> Rooms
+        {
+            get => _rooms;
+            set
+            {
+                _rooms = value;
+                OnPropertyChanged(nameof(Rooms));
+            }
+        }
 
         public RoomsViewModel()
         {
-            RoomsCollection = new ObservableCollection<RoomsViewModel>();
+            Rooms = new ObservableCollection<Room>();
             CreateRoomCommand = new Command(CreateRoom);            
-            CurrentRoom = new Room();   
         }
-        private void CreateRoom(object romObject)
-        {
-            if (romObject != null)
-            {
-                RoomsViewModel room = romObject as RoomsViewModel;
-                if (room != null)
-                {
-                    RoomsCollection.Add(room);
-                }                
-            }
-        }
-        
 
-        public int Number
+        private void CreateRoom()
         {
-            get { return CurrentRoom.Number; }
-            set
+            var room = new Room
             {
-                if (CurrentRoom.Number != value)
-                {
-                    CurrentRoom.Number = value;
-                    OnPropertyChanged("Number");
-                }
-            }
+                Number = this.Number,
+                FreePlace = this.FreePlace
+            };
+
+            Rooms.Add(room);
         }
-        public int FreePlace
-        {
-            get { return CurrentRoom.FreePlace; }
-            set
-            {
-                if (CurrentRoom.FreePlace != value)
-                {
-                    CurrentRoom.FreePlace = value;
-                    OnPropertyChanged("FreePlace");
-                }
-            }
-        }
+
+        #region INotifyPropertyChanged implementation
 
         public event PropertyChangedEventHandler PropertyChanged;
         protected void OnPropertyChanged(string propName)
         {
-            if (PropertyChanged != null)
-                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
         }
+
+        #endregion
     }
 }
